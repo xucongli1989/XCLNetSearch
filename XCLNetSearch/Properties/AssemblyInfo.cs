e880fd0d6863438dc3b6c807c6084c837105544c
xucongli1989 <80213876@qq.com>
2017-07-15 19:44:27 +0800

    添加nuget配置

--- XCLNetSearch/Properties/AssemblyInfo.cs
+++ XCLNetSearch/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
-[assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.*")]
+[assembly: AssemblyFileVersion("1.0.2")]
 [assembly: System.Web.UI.WebResource("XCLNetSearch.Style.Images.add_search.gif", "img/jpeg")]
 [assembly: System.Web.UI.WebResource("XCLNetSearch.Style.Images.del_search.gif", "img/jpeg")]
 [assembly: System.Web.UI.WebResource("XCLNetSearch.Style.Images.down.gif", "img/jpeg")]
